5e887d26e4c59dfc3a4f2d52b217bcbf814652e1
Salih Arya Gumilang <saliharyazinc@gmail.com>
2022-07-16 02:33:28 +0700

    form validation

--- dist/js/script.js
+++ dist/js/script.js
@@ -16,3 +16,22 @@ hamburger.addEventListener('click', function() {
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
 });
+
+const name = document.querySelector('#name')
+const email = document.querySelector('#email')
+const adress = document.querySelector('#adress')
+
+function validateForm() {
+    if (name.value == '') {
+        alert('Please fill name field')
+        return false
+    }
+    if (email.value == '') {
+        alert('Please fill email field')
+        return false
+    }
+    if (adress.value == '') {
+        alert('Please fill adress field')
+        return false
+    }
+}
